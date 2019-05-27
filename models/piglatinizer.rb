87792b31ecdf6eac6ad9252e694be9bd9dafd0a1
Eyal Toledano <eutait@gmail.com>
2019-05-27 13:53:00 -0400

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,26 +7,30 @@ class PigLatinizer
     @phrase = phrase
   end
 
-  def piglatinize(word)
-    letters = word.split('')
-
-    slicer = []
-    letters.each do |letter|
-      binding.pry
-      if letter.match(/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXZ]/)
-        slicer << letter
-      end
+  def piglatinize(words) # words
+    if words.include?(" ")
+      single_words = words.split(" ") # if there's more than one word
+    else
+      single_words = []
+      single_words << words
     end
 
-    binding.pry
-
-
-    # word = word.join
-    # unless append.empty?
-    #   word = word + append + "ay"
-    # else
-    #   word = word + "way"
-    # end
+    sentence = single_words.collect do |word|
+      binding.pry
+      letters = word.split('')
+      first_vowel = letters.detect {|letter| letter.match(/[AEIOUYaeiouy]/)}
+      binding.pry
+      first_vowel_index = letters.index(first_vowel)
+      chunk1 = letters.slice(first_vowel_index..-1) # => ["o", "r", "k"]
+      chunk2 = letters - chunk1 # => ["p"]
+      binding.pry
+      unless chunk2.empty?
+        word = chunk1.join + chunk2.join + "ay" # => "orkpay"
+      else
+        word = chunk1.join + "way" # => "Iway"
+      end
+    end
+    sentence.count == 1 ? sentence[0] : sentence.join(" ")
   end
 
 end
