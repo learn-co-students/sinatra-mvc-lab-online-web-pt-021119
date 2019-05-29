a522738fd421bb35ae97441ab3e3cdf2fefba661
Tracy Ngo <tngo1206@gmail.com>
2019-05-28 19:44:25 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    text = PigLatinizer.new
+    @piglatinized = text.piglatinize(params[:user_phrase])
+    erb :results
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,33 @@
+class PigLatinizer
+
+  def piglatinize(user_text)
+    user_text.split(' ').length == 1 ? piglatinize_word(user_text) : piglatinize_phrase(user_text)
+  end
+
+  def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
+  end
+
+  def piglatinize_word(word)
+    # word starts with vowel
+    if !consonant?(word[0])
+      word = word + "w"
+    # word starts with 3 consonants
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+    # word starts with 2 consonants
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
+    # word starts with 1 consonant
+    else
+      word = word.slice(1..-1) + word.slice(0)
+    end
+    word << "ay"
+  end
+
+  def piglatinize_phrase(sentence)
+    sentence.split.map do |word|
+      piglatinize_word(word)
+    end.join(' ')
+  end
+end
